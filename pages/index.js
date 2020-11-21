39f340090b7d4731d8714b7855808009384f902d
mehmet.erturk <mehmet.erturk@leanscale.com>
2020-11-21 22:56:51 +0200

    next entry

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mehmet Erturk</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          You will be welcome when I finish this!
+        </h1>
+
+        <p className={styles.description}>
+          But meanwhile, you can find me from any social media!{' '}
+          <br></br><code className={styles.code}>but where?</code>
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+          <p className={styles.description} >Once upon a time, I had time to do things..</p>
+      </footer>
+    </div>
+  )
+}
